37f7a431624f0a3d174538f36f848a1a979a676b
karthikeyan20032003@gmail.com <karthikeyan20032003@gmail.com>
2023-11-19 07:21:28 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def Name_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
