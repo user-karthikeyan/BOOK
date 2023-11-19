0d8e02bb46e7ac44ee43294764b41aa82b68e1ab
karthikeyan20032003@gmail.com <karthikeyan20032003@gmail.com>
2023-11-19 10:17:20 +0000

    Created Form1.RowTemplate1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,7 +4,6 @@ import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    
     self.init_components(**properties)
 
   def go_click(self, **event_args):
@@ -20,6 +19,12 @@ class Form1(Form1Template):
     self.image_4.source = books[3]['url']
     self.image_5.source = books[4]['url']
     self.image_6.source = books[5]['url']
+    self.title_1.content = books[0]['title']
+    self.title_2.content = books[1]['title']
+    self.title_3.content = books[2]['title']
+    self.title_4.content = books[3]['title']
+    self.title_5.content = books[4]['title']
+    self.title_6.content = books[5]['title']
     self.Book_1.visible = True
     self.Book_2.visible = True
     self.Book_3.visible = True
