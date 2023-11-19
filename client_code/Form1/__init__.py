6fd4a4ceaae6d9d871ec22c313136c9ae0b6bdce
karthikeyan20032003@gmail.com <karthikeyan20032003@gmail.com>
2023-11-19 09:12:11 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,13 +1,28 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  def go_click(self, **event_args):
+    books = anvil.server.call('recommend', self.BookName.text)
 
-  def Name_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
+    if books:
+      self.render_result(books)
+      
+  def render_result(self, books):
+    self.image_1.source = books[0]['url']
+    self.image_2.source = books[1]['url']
+    self.image_3.source = books[2]['url']
+    self.image_4.source = books[3]['url']
+    self.image_5.source = books[4]['url']
+    self.image_6.source = books[5]['url']
+    self.Book_1.visible = True
+    self.Book_2.visible = True
+    self.Book_3.visible = True
+    self.Book_4.visible = True
+    self.Book_5.visible = True
+    self.Book_6.visible = True
